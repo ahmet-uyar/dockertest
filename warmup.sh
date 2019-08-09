05a213a2ae4c8ae61f57400375c789329573fd1e
root <root@j-042.juliet.futuresystems.org>
2019-08-09 07:44:09 -0400

    sudo command removed, for loop limit changed

--- warmup.sh
+++ warmup.sh
@@ -11,22 +11,22 @@ sleepTime=10
 
 images="openwhisk/action-php-v7.3:nightly openwhisk/java8action:nightly openwhisk/python3action:nightly openwhisk/action-dotnet-v2.2:nightly openwhisk/action-ruby-v2.5:nightly openwhisk/action-swift-v4.2:nightly openwhisk/actionloop-golang-v1.11:nightly"
 
-for i in {0..5}
+for i in {0..4}
 do
   for image in $images
   do
     echo "Starting the warmup for: $image"
     start=$(date +%s%3N)
-    contId=$(sudo docker run -d $image)
+    contId=$(docker run -d $image)
     end=$(date +%s%3N)
     delay=$((end-start))
     echo "$image $delay" >> $outFile
 
     echo "container started with id: $contId"
     echo "Stopping the container ..."
-    sudo docker stop $contId
+    docker stop $contId
     echo "Removing the container ..."
-    sudo docker rm $contId
+    docker rm $contId
 
     echo "Wait $sleepTime seconds before warming up the next container"
     sleep $sleepTime
